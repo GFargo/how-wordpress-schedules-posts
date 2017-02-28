54597144d45024c60056361c9a9e706cca97a303
Griffen Fargo <griffen@alleyinteractive.com>
2017-02-28 11:20:45 -0500

    updates to slideshow - working structure in place

--- presentation/index.js
+++ presentation/index.js
@@ -9,6 +9,7 @@ import {
 	CodePane,
 	Deck,
 	Fill,
+	Fit,
 	Heading,
 	Image,
 	Layout,
@@ -50,6 +51,7 @@ const images = {
 	scheduled_for: require("../assets/scheduledfor.jpg"),
 	scheduling_post: require("../assets/scheduling.jpg"),
 	wp_cron_trac: require("../assets/wp_cron_trac.jpg"),
+	crontab_guru: require("../assets/crontab_guru.jpg"),
 	dif_time_servers: require("../assets/dif_time_servers.jpg"),
 	nist_time_servers: require("../assets/nist_time_servers.jpg"),
 	publish_notifications: require("../assets/publish_notifications.jpg"),
@@ -66,16 +68,22 @@ const gifs = {
 	loop_wargames: require("../assets/loop_wargames.gif"),
 	loop_zoidberg: require("../assets/loop_zoidberg_blinking_futurama.gif"),
 	loop_houses: require("../assets/loop_houses.gif"),
+	loop_nerds: require("../assets/loop_nerds.gif"),
+	loop_coffee_house: require("../assets/loop_coffee_house.gif"),
 	ready_for_this: require("../assets/ready_for_this.gif"),
 	pass_out: require("../assets/pass_out.gif"),
 	time_is_abstract: require("../assets/time_is_abstract.gif"),
 	frantic: require("../assets/frantic.gif"),
 	timelord: require("../assets/timelord.gif"),
+	traffic_jam: require("../assets/traffic_jam.gif"),
 	whats_happening: require("../assets/whats_happening.gif"),
 	waiting: require("../assets/waiting.gif"),
+	waiting_spongebob: require("../assets/waiting_spongebob.gif"),
 }
 
 preloader(images);
+
+preloader(gifs);
 
 const theme = createTheme({
 	// primary: "#ff4081"
@@ -134,6 +142,9 @@ export default class Presentation extends React.Component {
 
 					<Slide transition={["fade"]} bgImage={images.city_04.replace("/", "")}>
 						<Image transition={["fade"]} src={gifs.time_is_abstract.replace("/", "")} width="100%" />
+						<Heading size={1} fit caps lineHeight={1} textColor="white">
+							How Robots Keep Track of Time
+						</Heading>
 					</Slide>
 
 					<Slide transition={["fade"]} bgColor="white" bgImage={images.city_04.replace("/", "")}>
@@ -171,23 +182,21 @@ export default class Presentation extends React.Component {
 
 
 
-
-
-
-					<Slide transition={["fade"]} bgColor="light_blue">
+					<Slide transition={["fade"]} bgImage={images.city_02.replace("/", "")}>
+						<Heading size={1} caps lineHeight={1} textColor="black">The Edit Post Screen</Heading>
 						<Image transition={["fade"]} src={images.edit_post.replace("/", "")} width="100%" />
 					</Slide>
 
-					<Slide transition={["fade"]} bgColor="primary">
+					<Slide transition={["fade"]} bgImage={images.city_02.replace("/", "")}>
 							<Heading size={1} caps lineHeight={1} textColor="white">❄️</Heading>
-							<Heading size={1} fit caps lineHeight={1} textColor="white">Custom Workflows</Heading>
-							<Text textColor="light_grey" bold>
-								Occasionally clients will use plugins or custom functionality in the theme to achieve their desired workflow
-							</Text>
+							<Heading size={1} fit caps lineHeight={1} textColor="black">Custom Workflows</Heading>
 					</Slide>
 
-					<Slide transition={["fade"]} bgColor="light_blue">
+					<Slide transition={["fade"]} bgImage={images.city_04.replace("/", "")}>
 						<Image transition={["fade", "zoom"]} src={images.confirm_publish_post.replace("/", "")} width="100%" />
+						<Text lineHeight={1.25} textColor="white" bold>
+							Occasionally clients will use plugins or custom functionality in the theme to achieve their desired workflow
+						</Text>
 					</Slide>
 
 					<Slide transition={["fade"]} bgImage={images.city_04.replace("/", "")}>
@@ -201,10 +210,16 @@ export default class Presentation extends React.Component {
 					</Slide>
 
 					<Slide transition={["fade"]} bgImage={images.city_04.replace("/", "")}>
+						<Heading size={1} fit caps lineHeight={1} textColor="white">
+							A whole bunch of options...
+						</Heading>
 						<Image transition={["fade", "zoom"]} src={images.edit_flow_extras.replace("/", "")} width="100%" />
 					</Slide>
 
-					<Slide transition={["fade"]} bgColor="light_blue">
+					<Slide transition={["fade"]} bgImage={images.city_02.replace("/", "")}>
+						<Heading size={1} fit caps lineHeight={1} textColor="black">
+							Ready to Post
+						</Heading>
 						<Image transition={["fade", "zoom"]} src={images.edit_post_zoomed.replace("/", "")} width="100%" />
 					</Slide>
 
@@ -214,41 +229,84 @@ export default class Presentation extends React.Component {
 							</Heading>
 					</Slide>
 
+					<Slide transition={["fade"]} bgImage={gifs.loop_factory.replace("/", "")}>
+							<Heading size={1} fit caps lineHeight={1} textColor="black">
+									Reality
+							</Heading>
+					</Slide>
 
-					<Slide transition={["fade"]} bgColor="primary">
+
+					<Slide transition={["fade"]} bgImage={images.city_02.replace("/", "")}>
 							<Heading size={1} fit caps lineHeight={1} textColor="black">
 									WordPress Default Behavior
 							</Heading>
 							<Image transition={["fade", "zoom"]} src={images.wp_cron_trac.replace("/", "")} width="100%" />
 					</Slide>
 
-					<Slide transition={["fade"]} bgColor="primary">
+					<Slide transition={["fade"]} bgImage={images.city_02.replace("/", "")}>
 							<Heading size={1} fit caps lineHeight={1} textColor="black">
-
+								WordPress's Pseudo Cron
 							</Heading>
-							<Image transition={["fade", "zoom"]} src={images.wp_cron_trac.replace("/", "")} width="100%" />
+							<Image src={gifs.loop_wargames.replace("/", "")} />
+							<Heading size={1} fit caps lineHeight={1} textColor="white">
+								WP-Cron
+							</Heading>
+					</Slide>
+
+
+					<Slide transition={["fade"]} bgColor="primary"  bgImage={images.city_02.replace("/", "")}>
+							<Heading size={1} fit caps lineHeight={1} textColor="black">
+									Low Traffic v.s. High Traffic
+							</Heading>
+							<Layout>
+								<Fill>
+									<Image padding="24px" transition={["fade", "zoom"]} src={gifs.waiting_spongebob.replace("/", "")} height="275px" />
+								</Fill>
+								<Fill>
+									<Image padding="24px" transition={["fade", "zoom"]} src={gifs.traffic_jam.replace("/", "")} height="275px" />
+								</Fill>
+							</Layout>
+					</Slide>
+
+					<Slide transition={["fade"]} bgImage={images.city_02.replace("/", "")}>
+							<Heading size={1} fit caps lineHeight={1} textColor="black">
+									Alternative Solutions
+							</Heading>
+							<Image margin="32px auto 0" src={gifs.timelord.replace("/", "")} />
 					</Slide>
 
 					<Slide transition={["fade"]} bgColor="primary">
-							<Heading size={1} fit caps lineHeight={1} textColor="black">
+							<Heading size={1} fit caps lineHeight={1} textColor="white">
 									What is a Cron job?
 							</Heading>
 							<Text textColo="white" lineHeight={1.25}>
 								is a Linux utility which schedules a command or script on your server to run automatically at a specified time and date.
 							</Text>
+							<Appear fid="1">
+							  <Image padding="24px" transition={["fade", "zoom"]} src={gifs.loop_nerds.replace("/", "")} height="360px" />
+							</Appear>
 					</Slide>
 
-					<Slide transition={["fade"]} bgColor="primary">
-							<Heading size={1} fit caps lineHeight={1} textColor="black">
-									Low Traffic v.s. High Traffic
+
+					<Slide transition={["fade"]} bgImage={images.city_03.replace("/", "")}>
+							<Heading size={1} fit caps lineHeight={1} textColor="black" margin="0 0 24px">
+									Simple Example
 							</Heading>
+							<Layout>
+								<Fill>
+									<CodePane
+										padding="24px"
+								    source={require("raw!../assets/code/crontab.example")}
+									/>
+									<Image padding="24px" src={images.crontab_guru.replace("/", "")} height="275px" />
+								</Fill>
+							</Layout>
 					</Slide>
 
 
-
-					<Slide transition={["fade"]} bgImage={gifs.loop_factory.replace("/", "")}>
-							<Heading size={1} fit caps lineHeight={1} textColor="black">
-									Reality
+					<Slide transition={["fade"]} bgImage={images.city_01.replace("/", "")}>
+							<Heading size={1} fit caps lineHeight={1} textColor="white">
+									Our Hosting Solutions
 							</Heading>
 					</Slide>
 
@@ -259,11 +317,13 @@ export default class Presentation extends React.Component {
 							</Heading>
 					</Slide>
 
+
 					<Slide transition={["fade"]} bgImage={gifs.loop_simpsons_honking.replace("/", "")}>
 							<Heading size={1} fit caps lineHeight={1} textColor="white">
 									VIP Classic
 							</Heading>
 					</Slide>
+
 
 					<Slide transition={["fade"]} bgImage={gifs.loop_bb8.replace("/", "")}>
 							<Heading size={1} fit caps lineHeight={1} textColor="black">
@@ -271,18 +331,38 @@ export default class Presentation extends React.Component {
 							</Heading>
 					</Slide>
 
-					<Slide transition={["fade"]} bgImage={gifs.loop_wargames.replace("/", "")}>
-							<Heading size={1} fit caps lineHeight={1} textColor="black">
-									3rd Party Services
-							</Heading>
 
+					<Slide transition={["fade"]} bgImage={images.city_04.replace("/", "")}>
+						<Image padding="24px" transition={["fade", "zoom"]} src={gifs.loop_coffee_house.replace("/", "")} height="360px" />
+						<Heading size={1} fit caps lineHeight={1} textColor="white">
+								Additional Services
+						</Heading>
+						<Layout textColor="white">
+							<Fill>
+								<Heading textSize={42} caps>WordPress Plugins</Heading>
+								<List textColor="white">
+								  <ListItem>WP Cron-Control</ListItem>
+								  <ListItem>Cron-Control</ListItem>
+								</List>
+							</Fill>
+							<Fill>
+								<Heading textSize={42} caps>3rd Party Services</Heading>
+								<List textColor="white">
+								  <ListItem>EasyCron</ListItem>
+								  <ListItem>WebCron</ListItem>
+								  <ListItem>Dime a Dozen...</ListItem>
+								</List>
+							</Fill>
+						</Layout>
 					</Slide>
-
 
 					<Slide transition={["fade"]}  bgImage={gifs.loop_houses.replace("/", "")}>
 						<Heading size={1} fit caps lineHeight={1} textColor="white">
 								Thank you
 						</Heading>
+						<Text textColor="white">
+							Special thanks to Jared, Ben & Jake.
+						</Text>
 					</Slide>
 
 					<Slide transition={["fade"]} bgImage={gifs.loop_houses.replace("/", "")}>
